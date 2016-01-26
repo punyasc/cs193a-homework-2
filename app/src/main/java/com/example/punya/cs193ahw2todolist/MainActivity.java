651d7d5485ad233739fb70418faf9edff53d7711
Punya <punya@stanford.edu>
2016-01-25 22:00:17 -0800

    Updated the to-do list to highlight urgent tasks

--- app/src/main/java/com/example/punya/cs193ahw2todolist/MainActivity.java
+++ app/src/main/java/com/example/punya/cs193ahw2todolist/MainActivity.java
@@ -4,18 +4,23 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> taskArray;
+    Set <String> priorityArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +30,13 @@ public class MainActivity extends AppCompatActivity {
             if (savedInstanceState.getStringArrayList("taskArray") != null)
                 taskArray = savedInstanceState.getStringArrayList("taskArray");
         } else {
-            taskArray = new ArrayList<String>();
+            taskArray = new ArrayList<>();
         }
+        priorityArray = new HashSet<>();
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskArray);
         ListView list = (ListView) findViewById(R.id.listView);
         list.setAdapter(adapter);
-        list.setSelector(R.drawable.listselector);
         list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         list.setItemsCanFocus(false);
 
@@ -55,6 +60,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                changeBackgroundColor(position);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    public void changeBackgroundColor(int index) {
+        ListView list = (ListView) findViewById(R.id.listView);
+        String selectedTask = taskArray.get(index);
+        if (priorityArray.contains(selectedTask)) {
+            list.getChildAt(index).setBackgroundColor(Color.TRANSPARENT);
+            priorityArray.remove(selectedTask);
+        } else {
+            priorityArray.add(selectedTask);
+            list.getChildAt(index).setBackgroundColor(Color.YELLOW);
+        }
     }
 
     public void onSaveInstanceState(Bundle savedInstanceState) {
